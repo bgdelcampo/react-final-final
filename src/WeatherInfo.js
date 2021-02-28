e3acec62ef30ec5f52af10719dc301587e3df0a7
Lars Zortel <larszortel@Larss-MacBook-Air.local>
2021-02-28 19:04:47 +0100

    added unit convertion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -9,21 +10,25 @@ export default function WeatherInfo(props) {
 
                 <li className="text-capitalize">{props.data.description}</li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <div className="float-left">
-                        <WeatherIcon code={props.data.icon} />
+                    <div className="clearfix">
+                        <div className="float-left">
+                            <WeatherIcon code={props.data.icon} />
+                        </div>
+
+                        <div className="float-left">
+                            <WeatherTemperature celsius={props.data.temperature} />
+                        </div>
                     </div>
-                    <spam className="temperature">{Math.round(props.data.temperature)}</spam>°C
-                    </div>
+                </div>
                 <div className="col-6">
                     <ul>
-                        <li>Humidity:{props.data.humidity}%</li>
-                        <li>Wind:{Math.round(props.data.wind)}km/h</li>
+                        <li>Humidity: {props.data.humidity}%</li>
+                        <li>Wind: {props.data.wind} km/h</li>
                     </ul>
                 </div>
             </div>
-
         </div>
     )
 }
