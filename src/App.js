8c16d649339f4e043632f8823ceeb2fedfb4f9f8
Lars Zortel <larszortel@Larss-MacBook-Air.local>
2021-02-28 12:05:01 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+        <footer>
+          This project was coded by{" "}
+
+           Bea
+
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
